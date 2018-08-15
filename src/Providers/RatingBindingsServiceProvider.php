20b53b64bab9476099e3c8012aadf260884c308e
Konstantin Bukin <bukin@inetstudio.ru>
2018-08-15 16:15:18 +0300

    Change bindings provider

--- src/Providers/RatingBindingsServiceProvider.php
+++ src/Providers/RatingBindingsServiceProvider.php
@@ -3,33 +3,31 @@
 namespace InetStudio\Rating\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Foundation\Application;
 
 /**
  * Class RatingBindingsServiceProvider.
  */
 class RatingBindingsServiceProvider extends ServiceProvider
 {
+    /**
+    * @var  bool
+    */
     protected $defer = true;
 
-    public $bindings = [];
-
     /**
-     * RatingBindingsServiceProvider constructor.
-     *
-     * @param Application $app
-     */
-    public function __construct(Application $app)
-    {
-        parent::__construct($app);
-
-        $this->bindings = getPackageBindings(__DIR__.'/../Contracts');
-    }
+    * @var  array
+    */
+    public $bindings = [
+        'InetStudio\Rating\Contracts\Models\RatingModelContract' => 'InetStudio\Rating\Models\RatingModel',
+        'InetStudio\Rating\Contracts\Models\RatingTotalModelContract' => 'InetStudio\Rating\Models\RatingTotalModel',
+        'InetStudio\Rating\Contracts\Models\Traits\RateableContract' => 'InetStudio\Rating\Models\Traits\Rateable',
+        'InetStudio\Rating\Contracts\Services\RatingServiceContract' => 'InetStudio\Rating\Services\RatingService',
+    ];
 
     /**
      * Получить сервисы от провайдера.
      *
-     * @return array
+     * @return  array
      */
     public function provides()
     {
