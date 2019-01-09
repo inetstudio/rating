a12eac3ad92ca143525c5a99d6bed2f0f842ea37
Konstantin Bukin <bukin@inetstudio.ru>
2019-01-09 19:57:54 +0300

    Fix icons

--- resources/views/back/includes/navigation.blade.php
+++ resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
 <li class="{{ isActiveRoute('back.rating.*') }}">
-    <a href="{{ route('back.rating.index') }}"><i class="fa fa-thumbs-o-up"></i> <span class="nav-label">Рейтинг</span></a>
+    <a href="{{ route('back.rating.index') }}"><i class="fa fa-thumbs-up"></i> <span class="nav-label">Рейтинг</span></a>
 </li>
