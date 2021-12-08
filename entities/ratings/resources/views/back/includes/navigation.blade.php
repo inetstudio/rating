d6be244fc31fc18287021f9db3b2691c82f976d9
Konstantin Bukin <bukin@inetstudio.ru>
2021-12-08 11:18:53 +0300

    Update

--- entities/ratings/resources/views/back/includes/navigation.blade.php
+++ entities/ratings/resources/views/back/includes/navigation.blade.php
@@ -1,3 +1,3 @@
-<li class="{{ isActiveRoute('back.ratings-package.ratings.*') }}">
+<li class="{{ isActiveRoute('back.ratings-package.ratings.*', 'mm-active') }}">
     <a href="{{ route('back.ratings-package.ratings.index') }}"><i class="fa fa-thumbs-up"></i> <span class="nav-label">Рейтинг</span></a>
 </li>
